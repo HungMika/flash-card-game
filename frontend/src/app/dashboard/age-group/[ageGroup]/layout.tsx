31a512b464a9427f3ad808c1bdc504ab29c91cf1
Hung Dang <106718735+HungMika@users.noreply.github.com>
2025-04-02 13:36:56 +0700

    fix pấm

--- frontend/src/app/dashboard/age-group/[ageGroup]/layout.tsx
+++ frontend/src/app/dashboard/age-group/[ageGroup]/layout.tsx
@@ -1,16 +1,15 @@
 import { QuestionHeader } from '@/features/question/question-header';
 import React from 'react';
 
-export default async function AgeGroupLayout({
+export default function AgeGroupLayout({
   children,
   params,
 }: {
   children: React.ReactNode;
   params: { ageGroup: string };
 }) {
-  //const ageParams = await params;
   return (
-    <div className="p-6 space-y-4 ">
+    <div className="p-6 space-y-4">
       <QuestionHeader />
       {children}
     </div>
