b53356fc324bcdc84af98820a5565acabf0a3792
Hung Dang <106718735+HungMika@users.noreply.github.com>
2025-04-27 11:23:46 +0700

    fix bullshit auth

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,6 @@
-import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
-  const cookieStore = cookies();
-  const accessToken = (await cookieStore).get('accessToken');
-
-  if (!accessToken) {
-    redirect('/auth');
-  }
 
   redirect('/dashboard');
 }
